11e8395e0cb54507abcbf62fb409ce8030b6aba8
Sana Malik <ahsansana66@gmail.com>
2024-12-16 20:51:57 +0500

    Delete Lab # 6 Tasks directory

--- Lab # 6 Tasks/print no using for loop.cpp
+++
@@ -1,9 +0,0 @@
-//Print sequence of numbers: 2, 5, 8, 11, 14, 17, 20 using for loop.
-#include<iostream>
-using namespace std;
-int main(){
-	for( int i=2; i<=20; i+=3){
-		cout<< i << endl;
-	}
-	return 0;
-}
