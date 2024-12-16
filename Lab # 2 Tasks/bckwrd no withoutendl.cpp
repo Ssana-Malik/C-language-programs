ee86310a13238cca8852805c7932355d6dd7bfb9
Sana Malik <ahsansana66@gmail.com>
2024-12-16 20:51:21 +0500

    Delete Lab # 2 Tasks directory

--- Lab # 2 Tasks/bckwrd no withoutendl.cpp
+++
@@ -1,8 +0,0 @@
-/*Run Dev C++and create a new project and print the numbers in backward order from 10 to 1.
-b)With use of endl*/
-#include<iostream>
-using namespace std;
-int main(){
-	cout<<10<<" "<<9<<" "<<8<<" "<<7<<" "<<6<<" "<<5<<" "<<4<<" "<<3<<" "<<2<<" "<<1;
-return 0;	
-}
